4ffab90db7b056a3be365d15b29e776e7607d06b
Arina <arina2004popova@yandex.ru>
2022-01-17 16:04:28 +0300

    Исправлены ошибки

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import requests
-from pprint import pprint
-import requests
-from itertools import count
-import time
 from terminaltables import SingleTable
 
 languages = ['CSS', 'JavaScript', 'Java', 'C#', 'Ruby', 'PHP', 'C++', 'Python']
 
 
-TABLE_DATA = [
+TABLE_DATA_hh = [
         ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'],
     ]
 
+TABLE_DATA_sj = [
+        ['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата'],
+    ]
 
 def table_hh(language, vacancies_by_languages_hh):
-    TABLE_DATA.append([language, vacancies_by_languages_hh[language]['vacancies_found'],
+    TABLE_DATA_hh.append([language, vacancies_by_languages_hh[language]['vacancies_found'],
                        vacancies_by_languages_hh[language]['vacancies_processed'],
                        vacancies_by_languages_hh[language]['average_salary']])
 
-    return TABLE_DATA
+    return TABLE_DATA_hh
 
 
 def predict_rub_salary(vacancy):
@@ -30,13 +29,10 @@ def predict_rub_salary(vacancy):
         return int((vacancy['from'] + vacancy['to']) / 2)
 
 
-def head_hunter_table(languages, TABLE_DATA):
+def head_hunter_table(languages):
     title_hh = 'HeadHunter Moscow'
     vacancies_by_languages_hh = {}
     url_hh = 'https://api.hh.ru/vacancies'
-
-
-
 
     for language in languages:
         number_of_professions = 0
@@ -51,50 +47,18 @@ def head_hunter_table(languages, TABLE_DATA):
             page_response = requests.get(url_hh, params=param)
             page_response.raise_for_status()
             vacancies_by_page = page_response.json()
-            #pprint(vacancies_by_page)
-            #time.sleep(20)
-
-            #for vacancy_by_page in vacancies_by_page['items']:
-             #   print(vacancy_by_page)
-              #  #information_about_vacancies = vacancy_by_page['items']
 
             for vacancy in vacancies_by_page['items']:
                 salary_vacancy = vacancy['salary']
                 if salary_vacancy:
-                    #print(salary_vacancy)
                     if salary_vacancy['currency'] != 'RUR':
                         continue
                     average_salary += predict_rub_salary(salary_vacancy)
                     number_of_professions += 1
-                    # print(salary_vacancy)
-                    # print(predict_rub_salary(salary_vacancy))
-            # print(srednee)
+
             vacancies_by_languages_hh[language] = {'vacancies_found': proggramer_vacancies_by_language['found'],
                                                     'average_salary': average_salary // number_of_professions,
                                                     'vacancies_processed': number_of_professions}
-
-            #pprint(vacancies_by_page)
-            #time.sleep(3)
-        #pprint(proggramer_vacancies_by_language)
-        #time.sleep(20)
-
-
-        #for i in range(proggramer_vacancies_by_language['pages']):
-         #   proggramer_vacancies_by_language['page']
-
-        #for page in count(0):
-           # page_response = requests.get(url, params={'page': page})
-            #page_response.raise_for_status()
-           # page_data = page_response.json()
-            #if page >= page_data[proggramer_vacancies_by_language['pages']]:
-             #   break
-            #print(page_data)
-
-        #time.sleep(20)
-
-    #pprint(proggramer_vacancies_by_language)
-    #pprint(vacancies_by_languages)
-
 
         table_instance_hh = SingleTable(table_hh(language, vacancies_by_languages_hh), title_hh)
 
@@ -102,9 +66,13 @@ def head_hunter_table(languages, TABLE_DATA):
 
 
 
+
+
 def table_sj(language, vacancies_by_languages_sj):
-     TABLE_DATA.append([language, vacancies_by_languages_sj[language]['vacancies_found'], vacancies_by_languages_sj[language]['vacancies_processed'], vacancies_by_languages_sj[language]['average_salary']])
-     return TABLE_DATA
+     TABLE_DATA_sj.append([language, vacancies_by_languages_sj[language]['vacancies_found'],
+                        vacancies_by_languages_sj[language]['vacancies_processed'],
+                        vacancies_by_languages_sj[language]['average_salary']])
+     return TABLE_DATA_sj
 
 def predict_rub_salary_for_superJob(vacancy):
     if vacancy['currency'] == 'rub':
@@ -114,15 +82,13 @@ def predict_rub_salary_for_superJob(vacancy):
             return int(vacancy['payment_from'] * 1.2)
         return int((vacancy['payment_from'] + vacancy['payment_to']) / 2)
 
-def super_job_table(languages, TABLE_DATA):
+def super_job_table(languages):
     title_sj = 'SuperJob Moscow'
     vacancies_by_languages_sj = {}
-
 
     api = 'v3.r.135730693.e747e6daf76ce7b6302f4626a8eda784e6fe643d.82b2c4895662b62a11311215463b69c30479818d'
     d = {'X-Api-App-Id': api}
     url_sj = 'https://api.superjob.ru/2.0/vacancies'
-
 
     for language in languages:
         number_of_professions_sj = 0
@@ -133,21 +99,15 @@ def super_job_table(languages, TABLE_DATA):
             response = requests.get(url_sj, headers=d, params=param_sj)
             response.raise_for_status()
             super_job = response.json()
-            #pprint(super_job)
 
             for i in super_job['objects']:
                 if i['currency'] == 'rub' and i['payment_from'] != 0 or i['payment_to'] != 0:
                     curuncy += predict_rub_salary_for_superJob(i)
                     number_of_professions_sj += 1
-                    #print(i['profession'], i['town']['title'], curuncy)
 
                     vacancies_by_languages_sj[language] = {'vacancies_found': super_job['total'],
                                                         'average_salary': curuncy // number_of_professions_sj,
                                                         'vacancies_processed': number_of_professions_sj}
-
-
-
-            #print(super_job['more'])
 
             if super_job['more'] == False:
                 break
@@ -155,10 +115,10 @@ def super_job_table(languages, TABLE_DATA):
             param_sj['page'] += 1
         table_instance_sj = SingleTable(table_sj(language, vacancies_by_languages_sj), title_sj)
 
-
     return table_instance_sj.table
 
 
-print(head_hunter_table(languages, TABLE_DATA))
 
-print(super_job_table(languages, TABLE_DATA))
+print(head_hunter_table(languages))
+print()
+print(super_job_table(languages))
