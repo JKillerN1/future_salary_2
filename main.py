1f26272b4c540cc98425ad8e0ca58e3db933004e
Arina <arina2004popova@yandex.ru>
2022-02-27 16:50:24 +0300

    Изменены функции

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ from terminaltables import SingleTable
 
 def collect_table(language, table, vacancies_by_languages):
     table_copy = copy.copy(table)
-    if language in vacancies_by_languages:
-        table_copy.append([
-            language,
-            vacancies_by_languages[language]['vacancies_found'],
-            vacancies_by_languages[language]['vacancies_processed'],
-            vacancies_by_languages[language]['average_salary']])
+    if vacancies_by_languages[language]:
+        if language in vacancies_by_languages:
+            table_copy.append([
+                language,
+                vacancies_by_languages[language]['vacancies_found'],
+                vacancies_by_languages[language]['vacancies_processed'],
+                vacancies_by_languages[language]['average_salary']])
     return table_copy
 
 
 def predict_rub_salary(vacancy_from, vacancy_to):
-    if vacancy_from and vacancy_to:
-        if not vacancy_from:
-            return int(vacancy_to * 0.8)
-        if not vacancy_to:
-            return int(vacancy_from * 1.2)
-        return int((vacancy_from + vacancy_to) / 2)
+    if not vacancy_from:
+        return int(vacancy_to * 0.8)
+    if not vacancy_to:
+        return int(vacancy_from * 1.2)
+    return int((vacancy_from + vacancy_to) / 2)
 
 
 def get_table(languages, title, lang):
@@ -55,7 +55,7 @@ def get_sj_statistic(languages, token):
     lang = {}
     for language in languages:
         lang[language] = get_sj_statistic_of_lang(
-            language, token)
+            language)
     return lang
 
 
@@ -88,13 +88,13 @@ def get_hh_statistic_of_lang(language):
         vacancies_by_page = get_hh_vacancies(language, page)
         for vacancy in vacancies_by_page['items']:
             salary_vacancy = vacancy['salary']
-            if not salary_vacancy or salary_vacancy['currency'] != 'RUR':
-                continue
-            average_salary += predict_rub_salary(
-                salary_vacancy['from'],
-                salary_vacancy['to'])
-            number_of_professions += 1
-    if number_of_professions:
+            if salary_vacancy and salary_vacancy['currency'] == 'RUR':
+                average_salary += predict_rub_salary(
+                    salary_vacancy['from'],
+                    salary_vacancy['to'])
+                number_of_professions += 1
+        if not number_of_professions:
+            continue
         average_salary_for_profession = average_salary // number_of_professions
         vacancies_statistic = get_vacancies_statistic(
             hh_vacancies['found'],
@@ -121,27 +121,28 @@ def get_sj_statistic_of_lang(language):
     professions_sj_number = 0
     curuncy = 0
     page = 0
-    iter_count = count(start=0, step=10)
-    for number in iter_count:
+    for number in count(0, 1):
         sj_vacancies = get_sj_vacancies(language, page)
-        if sj_vacancies['objects']:
-            for vacancy in sj_vacancies['objects']:
-                if vacancy['currency'] == 'rub':
-                    if vacancy['payment_from'] or vacancy['payment_to']:
-                        curuncy += predict_rub_salary(
-                            vacancy['payment_from'],
-                            vacancy['payment_to'])
-                        professions_sj_number += 1
-            if not sj_vacancies['more']:
-                break
-            page += 1
-        if professions_sj_number:
-            average_salary_for_profession = curuncy // professions_sj_number
-            vacancies_statistic = get_vacancies_statistic(
-                sj_vacancies['total'],
-                average_salary_for_profession,
-                professions_sj_number)
-            return vacancies_statistic
+        if not sj_vacancies['objects']:
+            continue
+        for vacancy in sj_vacancies['objects']:
+            if vacancy['currency'] == 'rub':
+                if vacancy['payment_from'] or vacancy['payment_to']:
+                    curuncy += predict_rub_salary(
+                        vacancy['payment_from'],
+                        vacancy['payment_to'])
+                    professions_sj_number += 1
+        if not sj_vacancies['more']:
+            break
+        page += 1
+        if not professions_sj_number:
+            continue
+        average_salary_for_profession = curuncy // professions_sj_number
+        vacancies_statistic = get_vacancies_statistic(
+            sj_vacancies['total'],
+            average_salary_for_profession,
+            professions_sj_number)
+        return vacancies_statistic
 
 
 if __name__ == '__main__':
@@ -151,7 +152,7 @@ if __name__ == '__main__':
     sj_title = 'SuperJob Moscow'
     languages = ['CSS',
                  'JavaScript',
-                 'Java'
+                 'Java',
                  'C#',
                  'Ruby',
                  'PHP',
